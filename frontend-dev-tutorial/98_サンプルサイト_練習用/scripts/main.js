4ce1accc6a176832a1a8d7b9b2df64654061d3b1
YokotaYusuke <tarut8166@gmail.com>
2023-09-20 15:36:31 +0900

    Completed 練習用サイト

--- frontend-dev-tutorial/98_サンプルサイト_練習用/scripts/main.js
+++ frontend-dev-tutorial/98_サンプルサイト_練習用/scripts/main.js
@@ -1,21 +1,67 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const hero = new HeroSlider(".swiper");
-  hero.start();
+class Main {
+  constructor() {
+    this.header = document.querySelector(".header");
+    this._observers = [];
+    this.hero = new HeroSlider(".swiper");
+    this.sides = document.querySelectorAll(".side");
+    this._init();
+  }
 
-  const _textAnimation = function(el ,inview) {
+  _init() {
+    new MobileMenu;
+    Pace.on("done", this._scrollInit.bind(this));
+  }
+
+  _scrollInit() {
+    this._observers.push(
+      new ScrollObserver(".nav-trigger", this._navAnimation.bind(this), {once: false, rootMargin: "-300px 0px"}),
+      new ScrollObserver("#main-content", this._sideAnimation.bind(this), {once: false}),
+      new ScrollObserver(".swiper", this._toggleSlideAnimation.bind(this), {once: false}),
+      new ScrollObserver(".cover-slide", this._inviewAnimation),
+      new ScrollObserver(".appear", this._inviewAnimation),
+      new ScrollObserver(".tween-animate-title", this._textAnimation)    
+      );
+  }
+
+  _toggleSlideAnimation(el ,inview) {
     if(inview) {
-        const ta = new TweenTextAnimation(el);
-        ta.animate();
+        this.hero.start();
+    } else {
+        this.hero.stop();
     }
-}
-  const so = new ScrollObserver(".tween-animate-title", _textAnimation);
+  }
 
-  const _inviewAnimation = function(el ,inview) {
+  _navAnimation(el ,inview) {
+    if(inview) {
+        this.header.classList.remove("triggered");
+    } else {
+        this.header.classList.add("triggered");
+    }
+  }
+
+  _sideAnimation(el ,inview) {
+    if(inview) {
+        this.sides.forEach(side => side.classList.add("inview"));
+      } else {
+        this.sides.forEach(side => side.classList.remove("inview"));
+    }
+  }
+
+  _inviewAnimation(el ,inview) {
     if(inview) {
         el.classList.add("inview");
     } else {
         el.classList.remove("inview");
     }
+  }
+
+  _textAnimation(el ,inview) {
+    if(inview) {
+        const ta = new TweenTextAnimation(el);
+        ta.animate();
+    }
+  }
+
 }
-  const so2 = new ScrollObserver(".cover-slide", _inviewAnimation);
-});
+
+new Main();
